9dcd1a1f107be1066f1a1e2f829552d497b748dd
Pablo Zapeta <pzapeta@cooperativasalcaja.com>
2021-01-28 09:51:13 -0600

    modifcando yargs el TODO a to do

--- 04-por-hacer/config/yargs.js
+++ 04-por-hacer/config/yargs.js
@@ -2,14 +2,14 @@ const opttionsC = {
     description: {
         demand: true,
         alias: 'd',
-        desc: "description of TODO"
+        desc: "description of to do"
     },
 }
 const opttionsU = {
     description: {
         demand: true,
         alias: 'd',
-        desc: "description of TODO"
+        desc: "description of to do"
     },
     complete: {
         alias: 'c'
@@ -18,9 +18,9 @@ const opttionsU = {
 
 
 const argv = require('yargs')
-    .command('generate', "generate new TODO in database.", opttionsC)
-    .command('update', "update TODO in database.", opttionsU)
-    .command('list', "list TODO in database.")
+    .command('generate', "generate new to do in database.", opttionsC)
+    .command('update', "update to do in database.", opttionsU)
+    .command('list', "list to do in database.")
     .help()
     .argv; // Este argv deve ir siempre al final para poder hacer uso de las propiedades de el array _ -> investigar esta convencion
 
